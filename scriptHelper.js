915aaee0d1ace40f0254d20ddd9f0db119b9b2ee
EducatedStrikeCart <dhn004@gmail.com>
2021-08-19 09:23:15 -0400

    modified to fit grading tests

--- scriptHelper.js
+++ scriptHelper.js
@@ -32,57 +32,57 @@ function validateInput(testInput) { //This function is pointless but texbook wan
    if (testInput === '') {
        return "Empty";
    } else if (isNaN(testInput)) {
-        return "Not a number";
+        return "Not a Number";
    } else {
        return "Is a Number";
    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    if (pilot === '' || copilot === '' || fuelLevel === '' || cargoLevel === '') {
-        alert("All fields must be filled!")
-    }
-
     const faultyItems = document.getElementById("faultyItems")
     const launchStatus = document.getElementById("launchStatus")
 
+    if (pilot === '' || copilot === '' || fuelLevel === '' || cargoLevel === '') {
+        alert("All fields must be filled!")
+    } else {
+        faultyItems.style.visibility = "visible";
+    }
+
     let pilotStatus = document.getElementById("pilotStatus");
-    pilotStatus.innerText = "Pilot " + pilot + " Ready";
+    pilotStatus.innerText = "Pilot " + pilot + " is ready for launch";
 
 
     let copilotStatus = document.getElementById("copilotStatus");
-    copilotStatus.innerText = "Copilot " + copilot + " Ready";
+    copilotStatus.innerText = "Copilot " + copilot + " is ready for launch";
 
     if (validateInput(fuelLevel) !== "Is a Number") {
         alert("Please add a valid input for the Fuel Level!")
     } else if (Number(fuelLevel) < 10000) {
-        faultyItems.style.visibility = "visible";
         launchStatus.innerText = "Shuttle not ready for launch";
-        launchStatus.style.color = "red";
+        launchStatus.style.color = "rgb(199, 37, 78)";
 
         const fuelStatus = document.getElementById("fuelStatus");
         fuelStatus.innerText = "Not enough fuel for launch!"
-        fuelStatus.style.color = "red";
+        fuelStatus.style.color = "rgb(199, 37, 78)";
     }
 
 
     if (validateInput(cargoLevel) != "Is a Number") {
         alert("Please add a valid input for the Cargo Level!")
     } else if (Number(cargoLevel) > 10000) {
-        faultyItems.style.visibility = "visible";
         launchStatus.innerText = "Shuttle not ready for launch";
-        launchStatus.style.color = "red";
+        launchStatus.style.color = "rgb(199, 37, 78)";
         console.log(cargoLevel,typeof Number(cargoLevel))
 
         const cargoStatus = document.getElementById("cargoStatus");
         cargoStatus.innerText = "Cargo too heavy for launch!"
-        cargoStatus.style.color = "red";
+        cargoStatus.style.color = "rgb(199, 37, 78)";
     }
 
 
     if (fuelLevel >= 10000 && cargoLevel <= 10000) {
         launchStatus.innerText = "Shuttle is ready for launch"
-        launchStatus.style.color = "green";
+        launchStatus.style.color = "rgb(65, 159, 106)";
     }
 
     
